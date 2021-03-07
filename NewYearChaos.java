8df81c8b0ca0680476134b573a85cb7b25d35fa7
Manish Ranjan Behera <manish12042000@gmail.com>
2021-03-07 22:02:46 +0530

    New Year Chaos
    It is New Year's Day and people are in line for the Wonderland rollercoaster ride. Each person wears a sticker indicating their initial position in the queue from 1 to t. Any person can bribe the person directly in front of them to swap positions, but they still wear their original sticker. One person can bribe at most two others.
    
    Determine the minimum number of bribes that took place to get to a given queue order. Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.

---
+++ NewYearChaos.java
@@ -0,0 +1,75 @@
+/*It is New Year's Day and people are in line for the Wonderland rollercoaster ride. Each person wears a sticker indicating their initial position in the queue from 1 to n . Any person can bribe the person directly in front of them to swap positions, but they still wear their original sticker. One person can bribe at most two others.
+Determine the minimum number of bribes that took place to get to a given queue order. Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
+Function Description
+
+Complete the function minimumBribes in the editor below.
+
+minimumBribes has the following parameter(s):
+
+int q[n]: the positions of the people after all bribes
+Returns
+
+No value is returned. Print the minimum number of bribes necessary or Too chaotic if someone has bribed more than  people.
+Input Format
+
+The first line contains an integer t, the number of test cases.
+
+Each of the next t pairs of lines are as follows:
+- The first line contains an integer t, the number of people in the queue
+- The second line has n space-separated integers describing the final state of the queue.*/
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumBribes function below.
+    static void minimumBribes(int[] q) {
+        int bribes=0;
+        
+        for(int i=q.length -1;i>=0;i--){
+            if(q[i]-i-1>2){
+             System.out.println("Too chaotic");
+                return;
+            }
+            for(int j=Math.max(0,q[i]-2);j<i;j++){
+                if(q[j]>q[i]){
+                    bribes++;
+                }
+            }
+        }   
+        System.out.println(bribes);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] q = new int[n];
+
+            String[] qItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int qItem = Integer.parseInt(qItems[i]);
+                q[i] = qItem;
+            }
+
+            minimumBribes(q);
+        }
+
+        scanner.close();
+    }
+}
