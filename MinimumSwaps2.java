18fe55d69318dc84e5e28a29ca2f9a41d71e0e65
Manish Ranjan Behera <manish12042000@gmail.com>
2021-03-07 22:24:12 +0530

    Minimum Swaps 2
    You are given an unordered array consisting of consecutive integers  in [1, 2, 3, ..., n] without any duplicates. You are allowed to swap any two elements. You need to find the minimum number of swaps required to sort the array in ascending order.

---
+++ MinimumSwaps2.java
@@ -0,0 +1,75 @@
+/*You are given an unordered array consisting of consecutive integers  in [1, 2, 3, ..., n] without any duplicates. You are allowed to swap any two elements. You need to find the minimum number of swaps required to sort the array in ascending order.
+Function Description
+
+Complete the function minimumSwaps in the editor below. It must return an integer representing the minimum number of swaps to sort the array.
+
+minimumSwaps has the following parameter(s):
+
+arr: an unordered array of integers
+Input Format
+
+The first line contains an integer, n, the size of arr.
+The second line contains n space-separated integers arr[i] .
+
+Output Format
+
+Return the minimum number of swaps to sort the given array.*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+        int i =0;
+        int count=0;
+        int temp;
+        int  n = arr.length;
+        while(i<n){
+            if(arr[i] != i+1){
+                temp = arr[i];
+                arr[i] = arr[temp-1];
+                arr[temp-1]=temp;
+                count++;
+            }
+            else{
+                i++;
+            }
+        }
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = minimumSwaps(arr);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
