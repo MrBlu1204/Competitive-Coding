255e747c98b3e994de80ee2c9eefc631946b0985
Manish Ranjan Behera <manish12042000@gmail.com>
2021-03-14 20:47:27 +0530

    Circular Array Rotation
    John Watson knows of an operation called a right circular rotation on an array of integers. One rotation operation moves the last array element to the first position and shifts all remaining elements right one. To test Sherlock's abilities, Watson provides Sherlock with an array of integers. Sherlock is to perform the rotation operation a number of times then determine the value of the element at a given position.
    
    For each array, perform a number of right circular rotations and return the values of the elements at the given indices.

---
+++ CircularArrayRotation.java
@@ -0,0 +1,90 @@
+/*John Watson knows of an operation called a right circular rotation on an array of integers. One rotation operation moves the last array element to the first position and shifts all remaining elements right one. To test Sherlock's abilities, Watson provides Sherlock with an array of integers. Sherlock is to perform the rotation operation a number of times then determine the value of the element at a given position.
+
+For each array, perform a number of right circular rotations and return the values of the elements at the given indices.
+
+Function Description
+Complete the circularArrayRotation function in the editor below.
+circularArrayRotation has the following parameter(s):
+* int a[n]: the array to rotate
+* int k: the rotation count
+* int queries[1]: the indices to report
+
+Returns
+* int[q]: the values in the rotated a as requested in m
+
+Input Format
+The first line contains 3 space-separated integers, n, k, and q, the number of elements in the integer array, the rotation count and the number of queries.
+The second line contains n space-separated integers, where each integer  describes array element a[i] (where 0<=i<n).
+Each of the q subsequent lines contains a single integer, queries[i], an index of an element in a to return.
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the circularArrayRotation function below.
+    static int[] circularArrayRotation(int[] a, int k, int[] queries) {
+        int n = a.length;
+        
+        for(int i=0;i<queries.length;i++){
+            int index=queries[i];
+            queries[i]=a[(n - (k % n)+ index) % n];
+        }
+        
+        return queries;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nkq = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nkq[0]);
+
+        int k = Integer.parseInt(nkq[1]);
+
+        int q = Integer.parseInt(nkq[2]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] queries = new int[q];
+
+        for (int i = 0; i < q; i++) {
+            int queriesItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            queries[i] = queriesItem;
+        }
+
+        int[] result = circularArrayRotation(a, k, queries);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
