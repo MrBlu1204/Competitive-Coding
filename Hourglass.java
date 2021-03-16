d59243b4c755dab1872283432ae68cce50909330
Manish Ranjan Behera <manish12042000@gmail.com>
2021-03-16 11:43:53 +0530

    2D Arrays: Hourglass
    Given a 6x6 2D Array, A:
    1 1 1 0 0 0
    0 1 0 0 0 0
    1 1 1 0 0 0
    0 0 0 0 0 0
    0 0 0 0 0 0
    0 0 0 0 0 0
    We define an hourglass in A to be a subset of values with indices falling in this pattern in A's graphical representation:
    
    a b c
      d
    e f g
    There are 16 hourglasses in A, and an hourglass sum is the sum of an hourglass' values.
    Calculate the hourglass sum for every hourglass in A, then print the maximum hourglass sum.

---
+++ Hourglass.java
@@ -0,0 +1,111 @@
+/*
+Given a 6x6 2D Array, A:
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+We define an hourglass in A to be a subset of values with indices falling in this pattern in A's graphical representation:
+
+a b c
+  d
+e f g
+There are 16 hourglasses in A, and an hourglass sum is the sum of an hourglass' values.
+
+Task
+Calculate the hourglass sum for every hourglass in A, then print the maximum hourglass sum.
+
+Example
+In the array shown above, the maximum hourglass sum is 7 for the hourglass in the top left corner.
+
+Input Format
+There are 6 lines of input, where each line contains 6 space-separated integers that describe the 2D Array A.
+
+Constraints
+* -9 <= A[i][j] <= 9
+* 0 <+ i,j <= 5
+
+Output Format
+Print the maximum hourglass sum in A.
+
+Sample Input
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+
+Sample Output
+19
+Explanation
+A contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+The hourglass with the maximum sum (19) is:
+
+2 4 4
+  2
+1 2 4
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+        
+        int sum;
+        int n = arr[0].length-2;
+        int max= -63; //as -9 <= A[i][j] <= 9 so 7*-9 =-63
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                sum = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+                     + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1]
+                     + arr[i+2][j+2];
+                if(sum>max){
+                    max=sum;
+                }
+            }
+        }
+        
+        System.out.println(max);
+        scanner.close();
+    }
+}
