5417d34c7f46fb820381886a4bfde11575f3e91c
Manish Ranjan Behera <manish12042000@gmail.com>
2026-07-06 11:20:28 +0530

    0009-palindrome-number Math Easy

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -9,22 +9,16 @@ class Solution {
 
         long reverse = 0;
         int digit;
-        long num = x;
+        int num = x;
 
-        while(x !=0){
+        while(x > 0){
             digit = x%10;
             reverse = reverse*10 + digit;
 
             x /= 10;
         }
 
-        if(num == reverse){
-            return true;
-        }
-
-        return false;
-
-
+        return num==reverse;
         
     }
 }
