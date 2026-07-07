972b728d08f07501ac7b148263b16503520b7d3d
Manish Ranjan Behera <manish12042000@gmail.com>
2026-07-07 11:20:23 +0530

    0011-container-with-most-water Array Medium

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,14 +3,16 @@ class Solution {
         int maxWater = 0;
         int left =0;
         int right = height.length - 1;
+        int prevHeight;
 
         while(left < right){
             maxWater = Math.max(maxWater, (right - left) * Math.min(height[left], height[right]));
+            prevHeight = Math.min(height[left], height[right]);
 
-            if(height[left] < height[right]){
+            while(left < right  && height[left] <=prevHeight){
                 left++;
             }
-            else{
+            while(left < right  && height[right] <= prevHeight){
                 right--;
             }
         }
