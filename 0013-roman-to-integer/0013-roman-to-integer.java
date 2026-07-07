22328769368e070ed3d1742cf4f7dd813e34a8c6
Manish Ranjan Behera <manish12042000@gmail.com>
2026-07-07 12:05:45 +0530

    0013-roman-to-integer Hash Table Easy

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,28 +1,32 @@
 class Solution {
     public int romanToInt(String s) {
 
-        Map<Character, Integer> roman = new HashMap<>();
-        roman.put('I', 1);
-        roman.put('V', 5);
-        roman.put('X', 10);
-        roman.put('L', 50);
-        roman.put('C', 100);
-        roman.put('D', 500);
-        roman.put('M', 1000);
+    
 
         int num = 0;
 
         for(int i = 0; i < s.length() - 1; i++){
 
-            if(roman.get(s.charAt(i)) < roman.get(s.charAt(i+1))){
-                num -= roman.get(s.charAt(i));
+            if(roman(s.charAt(i)) < roman(s.charAt(i+1))){
+                num -= roman(s.charAt(i));
             } else {
-                num += roman.get(s.charAt(i));
+                num += roman(s.charAt(i));
             }
 
         }
         
-        return num + roman.get(s.charAt(s.length()-1));
+        return num + roman(s.charAt(s.length()-1));
         
+    }
+
+     static int roman(char ch){
+        if(ch == 'I') return 1;
+        if(ch == 'V') return 5;
+        if(ch == 'X') return 10;
+        if(ch == 'L') return 50;
+        if(ch == 'C') return 100;
+        if(ch == 'D') return 500;
+        if(ch == 'M') return 1000;
+        return 0;
     }
 }
