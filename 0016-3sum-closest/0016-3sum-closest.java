641266a85f4205bfd0d5e237ab06da99a0110d8d
Manish Ranjan Behera <manish12042000@gmail.com>
2026-07-09 11:17:24 +0530

    0016-3sum-closest Array Medium

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+
+        Arrays.sort(nums);
+        int result = Integer.MIN_VALUE;
+        int diff = Integer.MAX_VALUE;
+
+        for(int i = 0; i < nums.length -2; i++){
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            
+            int j =  i+1;
+            int k = nums.length - 1;
+
+            while(j<k){
+                int currSum = nums[i] + nums[j] + nums[k];
+                int currDiff = Math.abs(target - currSum);
+                if(currDiff<diff){
+                    result = currSum;
+                    diff = currDiff;
+                }
+                
+
+                if(currSum > target){
+                    k--;
+                }else if(currSum < target){
+                    j++;
+                }else{
+                    return currSum;
+                }
+            }
+
+        }
+
+        return result;
+        
+    }
+}
