338b1be9fc8294f4302dae698655f9dfc5f6b354
Manish Ranjan Behera <manish12042000@gmail.com>
2026-07-17 23:04:06 +0530

    0058-length-of-last-word String Easy

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,23 +1,18 @@
 class Solution {
     public int lengthOfLastWord(String s) {
 
-        if(!s.contains(" "))
-            return s.length();
+        s = s.trim();
 
-        int length = 0;
-        char[] str = s.toCharArray();
-        int i = str.length-1;
+        int count = 0;
 
-        while(str[i]== ' ' && i>=0){
-            i--;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ')
+                break;
+
+            count++;
         }
 
-        while(i>=0 && str[i] != ' '){
-            length++;
-            i--;
-        }
-
-        return length;
+        return count;
         
     }
 }
