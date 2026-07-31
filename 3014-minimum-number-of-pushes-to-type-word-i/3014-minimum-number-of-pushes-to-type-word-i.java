8249813749c14e15a3dd4c257528d7bd82519af9
Manish Ranjan Behera <manish12042000@gmail.com>
2026-07-31 12:50:42 +0530

    3014-minimum-number-of-pushes-to-type-word-i Math Easy

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumPushes(String word) {
+
+        int len = word.length();
+
+        int group = (len-1)/8 + 1;
+
+        int totalPushes =  group * (group-1) * 4 + (len - (group - 1) * 8) * group;
+
+        return totalPushes;
+
+        
+    }
+}
