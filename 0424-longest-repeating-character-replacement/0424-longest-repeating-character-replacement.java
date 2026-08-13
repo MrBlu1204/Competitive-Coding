a5f200b67da80f13a9d1785b508b4a0172a91eda
Manish Ranjan Behera <manish12042000@gmail.com>
2026-08-13 13:05:39 +0530

    0424-longest-repeating-character-replacement Hash Table Medium

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+        HashMap<Character,Integer> freq = new HashMap<>();
+
+        int left = 0, maxFreq = 0;
+
+        int result = 0;
+
+        for(int right = 0; right< s.length(); right++){
+            freq.put(s.charAt(right), freq.getOrDefault(s.charAt(right),0) + 1);
+            maxFreq = Math.max(maxFreq, freq.get(s.charAt(right)));
+
+            while((right - left + 1) - maxFreq > k){
+                freq.put(s.charAt(left), freq.get(s.charAt(left)) - 1);
+                left++;
+            }
+
+            result = Math.max(result, right - left + 1);
+        }
+
+        return result;
+        
+    }
+}
