945e574aa617916ffaea71d7eb6a78110318bc5e
Manish Ranjan Behera <manish12042000@gmail.com>
2026-08-14 13:06:23 +0530

    0076-minimum-window-substring Hash Table Hard

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,54 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        
+
+        if(s.length() < t.length() || t == null){
+            return "";
+        }
+
+        Map<Character,Integer> freqT = new HashMap<>();
+        Map<Character,Integer> freqWin = new HashMap<>();
+
+        for(char ch : t.toCharArray()){
+            freqT.put(ch, freqT.getOrDefault(ch, 0) + 1);
+        }
+
+        int match = 0, req = freqT.size();
+        int[] result = {-1,-1};
+        int resultLen =  Integer.MAX_VALUE;
+
+        int left = 0;
+
+        for(int right = 0; right < s.length(); right++){
+            char ch = s.charAt(right);
+
+            freqWin.put(ch, freqWin.getOrDefault(ch, 0) + 1);
+
+            if(freqT.containsKey(ch) && freqT.get(ch).equals(freqWin.get(ch))){
+                match++;
+            }
+
+            while(match == req){
+                if(right - left + 1 < resultLen){
+                    resultLen = right - left + 1;
+                    result[0] = left;
+                    result[1] = right;
+                }
+
+                ch = s.charAt(left);
+                freqWin.put(ch, freqWin.getOrDefault(ch, 0) - 1);
+
+                if(freqT.containsKey(ch) && freqT.get(ch) > (freqWin.get(ch))){
+                    match --;
+                }
+
+                left++;
+
+            }
+
+        }
+
+        return resultLen == Integer.MAX_VALUE ? "" : s.substring(result[0],result[1]+1);
+        
+    }
+}
