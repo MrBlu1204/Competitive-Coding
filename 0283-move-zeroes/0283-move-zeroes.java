6e6beea96c2100b362876f3abc821cfd03c45fdc
Manish Ranjan Behera <manish12042000@gmail.com>
2026-08-18 13:07:52 +0530

    0283-move-zeroes Array Easy

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0, j = 1;
+        int i = 0;
 
-        while(j< nums.length){
-            if(nums[i]==0 && nums[j]!=0){
+        for(int j = 0;j< nums.length; j++){
+            if(nums[j]!=0){
+                int temp = nums[i];
                 nums[i] = nums[j];
-                nums[j] = 0;
+                nums[j] = temp;
                 i++;
             }
-
-            if(nums[i] != 0){
-                i++;
-            }
-
-            j++;
         }
         
     }
