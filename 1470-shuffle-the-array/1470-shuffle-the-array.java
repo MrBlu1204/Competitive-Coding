f842bee9fcfa54a203c5dade4735e85ea8261f07
Manish Ranjan Behera <manish12042000@gmail.com>
2026-08-20 11:18:18 +0530

    1470-shuffle-the-array Array Easy

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,18 +1,17 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
 
-        int[] result = new int[2*n];
+       int max = 1001;  //1 <= nums[i] <= 10^3
 
-        int i = 0, j = 0, k = n;
+       for(int i = 0; i < n ; i++){
+        nums[2*i+1] += (nums[n+i]%max)*max;
+        nums[2*i] += (nums[i]%max)*max;
+       }
 
-        while(j < n){
-            result[i++] = nums[j++];
-            result[i++] = nums[k++];
-        }
-
-
-        return result;
-
+       for(int i = 0; i<2*n ;i++){
+        nums[i] /=max;
+       }
         
+        return nums;
     }
 }
