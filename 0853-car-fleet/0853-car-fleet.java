82fa4fa7c542fd2954228ae8654810e4506098f3
Manish Ranjan Behera <manish12042000@gmail.com>
2026-08-26 12:39:47 +0530

    0853-car-fleet Array Medium

---
+++ 0853-car-fleet/0853-car-fleet.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+
+        int n =  position.length;
+        int[][] cars = new int[n][2];
+
+        for(int i = 0; i < n; i++){
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        } 
+
+        Arrays.sort(cars,(a,b) -> Integer.compare(b[0],a[0]));
+
+        Deque<Double> fleets = new ArrayDeque<>();
+
+        for(int[] car : cars){
+            double time = (double)(target - car[0])/car[1];
+
+            while(fleets.isEmpty() || fleets.peek() < time){
+                fleets.push(time);
+            }
+        }
+
+        return fleets.size();
+        
+    }
+}
