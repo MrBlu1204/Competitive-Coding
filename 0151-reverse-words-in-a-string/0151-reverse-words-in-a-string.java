6d75e0c37be834c6bb2c2e0349b32cbdbbd24910
Manish Ranjan Behera <manish12042000@gmail.com>
2026-08-31 23:37:35 +0530

    0151-reverse-words-in-a-string Two Pointers Medium

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,11 +1,42 @@
 class Solution {
     public String reverseWords(String s) {
 
-        String[] words = s.trim().split("\\s+");
+        char[] str = s.toCharArray();
+        int n = str.length;
 
-        Collections.reverse(Arrays.asList(words));
+        reverse(str, 0, n-1);
 
-        return String.join(" ", words);
+
+
+        int write = 0;
+        for (int i = 0; i < n; i++) {
+            if (str[i] != ' ') {
+                if (write > 0) str[write++] = ' ';
+
+                int wordStart = write;
+                while (i < n && str[i] != ' ') {
+                    str[write++] = str[i++];
+            }
+            reverse(str, wordStart, write - 1);
+        }
+    }
+
+
+        return new String(str,0,write);
+
+
         
+    }
+
+
+    public void reverse(char[] str, int left, int right){
+        while(left<right){
+            char temp = str[left];
+            str[left] = str[right];
+            str[right] = temp;
+            left++;
+            right--;
+        }
+
     }
 }
