a209cfee2162186320b91eaab603456dfed6ffc7
Manish Ranjan Behera <manish12042000@gmail.com>
2026-09-03 12:50:03 +0530

    0875-koko-eating-bananas Array Medium

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int left = 1;
+        int right = Arrays.stream(piles).max().getAsInt();
+        int result = right;
+
+        while(left <= right){
+            int speed = left + (right - left)/2;
+            long totalTime = 0;
+
+            for(int pile : piles){
+                totalTime += Math.ceil((double) pile/speed);
+            }
+
+            if( totalTime <= h){
+                result = speed;
+                right = speed - 1;
+            }else{
+                left = speed + 1;
+            }
+        }
+
+        return result;
+        
+    }
+}
