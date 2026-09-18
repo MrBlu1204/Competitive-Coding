e770e41f31e490c07163774c154bb3a5dcea8ef2
Manish Ranjan Behera <manish12042000@gmail.com>
2026-09-18 12:20:26 +0530

    0137-single-number-ii Array Medium

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        HashMap<Integer,Integer> freq = new HashMap<>();
+
+        for(int num : nums){
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        for(int num : freq.keySet()){
+            if(freq.get(num) == 1){
+                return num;
+            }
+        }
+
+
+        return -1;
+        
+    }
+}
