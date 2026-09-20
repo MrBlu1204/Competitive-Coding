0ecfac6926553bfa013fd33785966df4cb9bac8c
Manish Ranjan Behera <manish12042000@gmail.com>
2026-09-20 23:19:06 +0530

    1189-maximum-number-of-balloons Hash Table Easy

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -7,9 +7,7 @@ class Solution {
             charCount[c - 'a'] += 1;
         }
 
-        int result = Integer.MAX_VALUE;
-
-        result = Math.min(result, charCount['b' - 'a']);
+        int result =  charCount['b' - 'a'];
         result = Math.min(result, charCount['a' - 'a']);
         result = Math.min(result, charCount['l' - 'a']/2);
         result = Math.min(result, charCount['o' - 'a']/2);
