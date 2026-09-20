eb59ecaefc7816aa9e78e87a81626e9ce106f9d4
Manish Ranjan Behera <manish12042000@gmail.com>
2026-09-20 23:32:53 +0530

    0383-ransom-note Hash Table Easy

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        if(ransomNote.length() > magazine.length()){
+            return false;
+        }
+
+        int[] charCount = new int[26];
+
+        for(char c : magazine.toCharArray()){
+            charCount[c - 'a']++;
+        }
+
+        for(char c : ransomNote.toCharArray()){
+            charCount[c - 'a']--;
+
+            if(charCount[c - 'a'] < 0){
+                return false;
+            }
+        }
+
+        return true;        
+    }
+}
